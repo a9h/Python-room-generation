eb42cfc0df659033d5cff53d1f6b60492977fcd5
Diamondwoody <69401362+Diamondwoody@users.noreply.github.com>
2021-10-18 01:02:45 +0100

    character class for health etc, just started adding item usage!

--- main.py
+++ main.py
@@ -4,6 +4,34 @@ import os
 haslooted = False
 r = ""
 doors = 0
+useable = ""
+inv = ["\nmedicine"]
+
+class character:
+    def __init__(self, currentHealth, maxHealth):
+        self.cHealth = currentHealth
+        self.mHealth = maxHealth
+    def giveHealth(self):
+        print(f"Your current health is {self.cHealth}")
+        
+player = character(100,100)
+
+
+class HealthPotion():
+    def act(self, player):
+        player.currentHealth += 20
+
+
+def use(item):
+    if item.lower() == "medicine":
+        player.mHealth += 20
+        print(f"Max health increased to {player.mHealth}")
+        inv.remove("\n" + item)
+        choices()
+    elif item.lower() == "water":
+        player.currentHealth += 5
+
+
 
 def startchoice():
     choice = input("> ")
@@ -39,18 +67,30 @@ def room():
 
 def loot(r):
     global haslooted
+    global doors
+
+
+    if doors == 1:
+        lucky = random.randint(1,101)
+
+
+    elif doors == 2:
+        lucky = random.randint(1,76)
+
+    elif doors == 3:  
+        lucky = random.randint(1,51)
+
     spots = ["You peaked in a cuboard", "You looked in a draw", "You opened a cabinate"]
     gathered = (random.choice(r["LootTables"]))
     
-    lucky = random.randint(1,101)
+
     if haslooted == False:
         if lucky < 33:
             haslooted = True
             print(f"{random.choice(spots)} and found {gathered}!")
 
-            with open ("inventory.json", "a") as f:
-                json.dump(gathered, f, indent=4)
-                choices()
+            inv.append(f"\n{gathered}")
+            choices()
                 
         else:
             haslooted = True
@@ -71,10 +111,14 @@ def loot(r):
 
 
 def choices():
+    global useable
     global doors
     choice = input("> ")
+
     if choice.lower() == "loot":
         loot(r)
+
+
     elif choice.lower() == "help":
         print("help - shows this menu")
         print("loot - look around your room for anything useful")
@@ -82,26 +126,64 @@ def choices():
         print("2 - goes through door 2 (If you have 2 doors available) ")
         print("3 - goes through door 2 (If you have 3 doors available) ")
         choices()
+
+
     elif choice.lower() == "1":
         os.system("clear")
         room()
         generation(r)
+
+
     elif choice.lower() == "2":
         if doors > 1:
             os.system("clear")
             room()
             generation(r) 
+
+
         else:
             print("You do not have 2 doors")
-            choices()        
+            choices()      
+
+
     elif choice.lower() == "3":
         if doors > 2:
             os.system("clear")
             room()
             generation(r) 
+
         else:
             print("You do not have 3 doors") 
             choices()
+
+
+    elif choice.lower() == "inventory" or choice.lower() == "inv":
+
+        print("You have: " + "".join(inv))
+        choices() 
+
+
+    elif choice.lower() == "use":
+        
+        print("You can use anything in your inventory " + "".join(inv))
+        useable = input("What would you like to use: ")
+        if ("\n" + useable) in inv:
+            use(useable)
+        else:
+            print("You do not have that item")
+            choices()
+    elif choice.lower() == "health":
+        player.giveHealth()
+        choices()
+
+
+        
+    else:
+        print("that is not a valid option")
+        choices()       
+
+
+
 
 
 
