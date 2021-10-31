cb4c088e3b747aee9c5178fc8e6f2fed94a20ee6
Diamondwoody <69401362+Diamondwoody@users.noreply.github.com>
2021-10-31 19:17:01 +0000

    Enemies added, hunger / thirst added, health completed

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 import random
 import json
+import time
 import os
 haslooted = False
 r = ""
 doors = 0
 useable = ""
+previous = True
 inv = ["\nmedicine"]
 
+
 class character:
-    def __init__(self, currentHealth, maxHealth):
+    def __init__(self, currentHealth, maxHealth, money, hunger, thirst):
         self.cHealth = currentHealth
         self.mHealth = maxHealth
+        self.money = money
+        self.hunger = hunger
+        self.thirst = thirst
     def giveHealth(self):
-        print(f"Your current health is {self.cHealth} and your max health is {self.mHealth}")
-        
-player = character(100,100)
+        print(f"Your current health is {self.cHealth} and your max health is {self.mHealth}\nYour thirst is {self.thirst} and your hunger is {self.hunger}\nYou have {self.money} pounds to your name")
+    def damageTook(self, damage):
+        self.cHealth -= damage
+        if self.cHealth == 0 or self.cHealth < 0:
+            print("Game over! you died")
+        else:
+            print(f"You lost {damage} health, and now have {self.cHealth} left")
 
+
+
+class weapon:
+    def __init__(self, durability):
+        self.durability = durability
+
+
+
+
+knifeWeapon = weapon(150)
+forkWeapon = weapon(40)
+batWeapon = weapon(60)
+
+
+
+
+
+
+
+
+
+
+player = character(100,100,50,100,100)
 class regularenemy:
   def __init__(self, health, strength):
     self.health = health
@@ -23,17 +56,102 @@ class regularenemy:
 
 
 
-def medicine():
-    player.mHealth += 20
-    print(f"Max health increased to {player.mHealth}")
-    inv.remove("\nmedicine")
-    choices()
 
 
-def use(item):
-    pass
 
 
+
+def use(item, encounter):
+    MaxHealthItems = ["medicine"]
+    CurrentHealthItems = ["bandage"]
+    weapons = ["knife", "fork", "bat", "torch", "crowbar", "branch", "shovel"]
+    hungerItems = ["waterbottle", "carrot", "tomato", "chocolate"]
+    thirstItems = ["tomato", "carrot", "waterbottle", "chocolate"]
+
+    with open ("stats.json", "r") as f:
+        data = json.load(f)
+
+        if item in weapons:
+            print("You cant use a weapon when there is no enemies to use it on")
+            if encounter == True:
+                encounterChoice()
+            else:
+                choices()
+
+
+
+
+        elif item in hungerItems and item in thirstItems:
+            findThirst = data["thirst"]
+            findHunger = data["hunger"]
+            randomHunger = random.choice(findHunger[item])
+            randomThirst = random.choice(findThirst[item])
+
+            hungerDifference = player.hunger - 100
+            thirstDifference = player.thirst - 100
+
+            if randomThirst > thirstDifference or randomHunger > hungerDifference:
+                player.thirst += thirstDifference
+                player.hunger += hungerDifference
+
+            else:
+                player.hunger += randomHunger
+                player.thirst += randomThirst
+            print(f"you used {item} and gained {randomHunger} hunger and {randomThirst} thirst")
+            print(f"your current hunger and thirst levels are {player.hunger} and {player.thirst}")
+
+            if encounter == True:
+                encounterChoice()
+            else:
+                choices()
+
+
+
+
+
+
+
+
+
+
+
+
+        elif item in MaxHealthItems:
+            FindOption = data["maxhealth"]
+            ToAdd = random.choice(FindOption[item])
+            player.mHealth += ToAdd
+            inv.remove("\n"+item)
+            print(f"Max health increased to {player.mHealth}")
+            if encounter == True:
+                encounterChoice()
+            else:
+                choices()
+
+        elif item in CurrentHealthItems:
+            FindOption2 = data["currenthealth"]
+
+            ToAdd2 = random.choice(FindOption2[item])
+
+
+
+
+
+            difference = player.mHealth - player.cHealth
+
+            if ToAdd2 > difference:
+                player.cHealth += difference
+
+                print(f"current health increased to {player.cHealth}")
+
+            else:
+                player.cHealth += ToAdd2
+                print(f"current health increased to {player.cHealth}")
+
+            inv.remove("\n" + item)
+            if encounter == True:
+                encounterChoice()
+            else:
+                choices()
 
 def startchoice():
     choice = input("> ")
@@ -54,16 +172,22 @@ def startscreen():
 
 
 
+
+
+
+
 # "Create" current room
 def room():
+
     global haslooted
     global doors
-    global r  
+    global r
     with open("rooms.json") as f:
         haslooted = False
-        data = json.load(f)  
+        data = json.load(f)
         r = random.choice(data["rooms"])
         doors = (random.randint(1,3))
+
 
 
 
@@ -79,12 +203,12 @@ def loot(r):
     elif doors == 2:
         lucky = random.randint(1,76)
 
-    elif doors == 3:  
+    elif doors == 3:
         lucky = random.randint(1,51)
 
-    spots = ["You peaked in a cuboard", "You looked in a draw", "You opened a cabinate"]
+    spots = ["You peaked in a cupboard", "You looked in a draw", "You opened a cabinet"]
     gathered = (random.choice(r["LootTables"]))
-    
+
 
     if haslooted == False:
         if lucky < 33:
@@ -93,15 +217,15 @@ def loot(r):
 
             inv.append(f"\n{gathered}")
             choices()
-                
+
         else:
             haslooted = True
-            print("Looks like you didnt find anything, unlucky")
+            print("Looks like you didn't find anything, unlucky")
             choices()
     else:
         print("You have already looted this room!")
         choices()
-    
+
 
 
 
@@ -140,54 +264,233 @@ def choices():
         if doors > 1:
             os.system("clear")
             room()
-            generation(r) 
+            generation(r)
 
 
         else:
             print("You do not have 2 doors")
-            choices()      
+            choices()
 
 
     elif choice.lower() == "3":
         if doors > 2:
             os.system("clear")
             room()
-            generation(r) 
+            generation(r)
 
         else:
-            print("You do not have 3 doors") 
+            print("You do not have 3 doors")
             choices()
 
 
     elif choice.lower() == "inventory" or choice.lower() == "inv":
 
         print("You have: " + "".join(inv))
-        choices() 
+        choices()
 
 
     elif choice.lower() == "use":
         if inv == False:
           print("You dont have anything in  your inventory!")
           choices()
+
         else:
+
           print("You can use anything in your inventory " + "".join(inv))
           useable = input("What would you like to use: ")
+
           if ("\n" + useable) in inv:
-              use(useable)
+              use(useable, False)
+
           else:
+
               print("You do not have that item")
               choices()
+
+
+
     elif choice.lower() == "health":
         player.giveHealth()
         choices()
 
 
-        
+
     else:
         print("that is not a valid option")
-        choices()       
+        choices()
 
 
+
+
+
+
+
+
+
+
+
+def encounterChoice():
+
+
+
+
+
+
+    global previous
+
+    choice = input("> ")
+
+
+    if choice.lower() == "run":
+
+        didEscape = random.randint(1,100)
+        if 30 > didEscape:
+            damageDealt = random.randint(2,30)
+            player.damageTook(damageDealt)
+            print("While trying to escape and the monster followed you")
+            encounterChoice()
+
+
+
+
+
+        else:
+            room()
+            print("You ran and escaped to another room!")
+            time.sleep(1)
+            previous = True
+            generation(r)
+
+
+    elif choice.lower() == "use":
+
+        if inv == False:
+          print("You don't have anything in  your inventory!")
+          encounterChoice()
+
+        else:
+
+          print("You can use anything in your inventory " + "".join(inv))
+          useable = input("What would you like to use: ")
+
+          if ("\n" + useable) in inv:
+              use(useable, True)
+
+          else:
+
+              print("You do not have that item")
+              encounterChoice()
+
+
+
+    elif choice.lower() == "fight":
+        weaponInv = []
+        for x in inv:
+
+            weaponList = ["\nknife", "\nfork", "\nbat", "\ntorch", "\ncrowbar", "\nbranch", "\nshovel"]
+            if x in weaponList:
+
+                weaponInv.append(x)
+
+        if weaponInv == []:
+            damageTaken = random.randint(5,35)
+            print(f"You don't have any weapons to attack with, and lost {damageTaken} health")
+            player.cHealth -= damageTaken
+            print("Pro tip - next time RUN if you don't have any weapons")
+            encounterChoice()
+
+
+
+
+
+        else:
+
+
+            enemyhealth = 100
+            firstTime = True
+            while enemyhealth > 0:
+
+                if firstTime == True:
+                    print("you have:" + "".join(weaponInv))
+                    firstTime = False
+                weaponChoice = input("What weapon do you want to attack the enemy with? > ")
+                print("")
+
+
+
+
+                if ("\n" + weaponChoice) in weaponInv:
+
+
+                    with open("weapons.json", "r") as f:
+                        data = json.load(f)
+
+                        Find = data["weapons"]
+                        damageToEnemy = random.choice(Find[weaponChoice])
+                        enemyhealth -= damageToEnemy
+                        if enemyhealth == 0 or 0 > enemyhealth:
+                            print(f"You did {damageToEnemy} to the enemy with your {weaponChoice} it now has 0 remaining")
+                        else:
+                            print(f"You did {damageToEnemy} to the enemy with your {weaponChoice}, the enemy now has {enemyhealth} remaining")
+
+                elif ("\n" + weaponChoice) not in weaponInv:
+                    healthLost = random.randint(5, 35)
+                    print(f"You spent too long looking for a weapon you don't have, and lost {healthLost} health")
+                    encounterChoice()
+
+
+
+                if enemyhealth > 0:
+                    damageTaken = random.randint(1,20)
+                    player.cHealth -= damageTaken
+                    print(f"The enemy slashed you and dealt {damageTaken} damage. You now have {player.cHealth} health remaining")
+                    print("")
+
+
+
+
+
+
+
+
+            print("You killed the enemy and ran to another room!")
+            time.sleep(2)
+            room()
+            generation(r)
+            previous = True
+
+
+
+    elif choice == "inv" or choice == "inventory":
+        print("You have: " + "".join(inv))
+        encounterChoice()
+
+
+
+
+    elif choice.lower() == "health":
+        player.giveHealth()
+        encounterChoice()
+
+
+
+    else:
+        print("That is not a valid command!")
+        encounterChoice()
+
+
+
+
+
+
+
+
+def randomEncounter():
+
+    os.system("clear")
+    print("Uh Oh, you have come across an enemy!")
+    print("you can RUN, FIGHT or USE an item.")
+    encounterChoice()
 
 
 
@@ -195,14 +498,51 @@ def choices():
 
 
 def generation(room):
-    global doors
+    hungerItems = ["waterbottle", "carrot", "tomato", "chocolate"]
+    foodinv = []
+
+
+    removeHunger = random.randint(1,100)
+    if removeHunger > 50:
+        hungerRemoved = random.randint(1,15)
+        thirstRemoved = random.randint(1,15)
+
+        player.thirst -= thirstRemoved
+        player.hunger -= hungerRemoved
+
+    if player.hunger == 0 or player.hunger < 0 or player.thirst == 0 or player.thirst < 0:
+        print("You ran out of hunger/thirst and died...")
+        print("Game over!")
+
+
+    if player.hunger < 20 or player.thirst < 20:
+        print("Your hunger and thirst levels are low. You should probably eat something")
+
+
+
+    global previous
+
+    encounterChoice = random.randint(1,100)
+    if 25 > encounterChoice and previous == False:
+        encounter = True
+    else:
+        encounter = False
+        previous = False
+
+
+
     isTrue = True
-    if isTrue == True:
+
+    if encounter == True:
+        randomEncounter()
+
+
+    elif isTrue == True and encounter == False:
         os.system("clear")
         realname = (r["RoomName"])
 
 
-        print(f"you find yourself in {realname} a  with {doors} door(s)")
+        print(f"you find yourself in a {realname} with {doors} door(s)")
         choices()
 
 
